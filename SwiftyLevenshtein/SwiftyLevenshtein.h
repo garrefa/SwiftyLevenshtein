f18c7394201855b8eecba82b11cd9a6b849a09ba
Alexandre Garrefa <alexandre.marcelo.ribeiro.garrefa@ing.com>
2018-07-10 21:41:38 +0200

    Initial Commit

---
+++ SwiftyLevenshtein/SwiftyLevenshtein.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLevenshtein.h
+//  SwiftyLevenshtein
+//
+//  Created by Alex on 10/07/2018.
+//  Copyright © 2018 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyLevenshtein.
+FOUNDATION_EXPORT double SwiftyLevenshteinVersionNumber;
+
+//! Project version string for SwiftyLevenshtein.
+FOUNDATION_EXPORT const unsigned char SwiftyLevenshteinVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLevenshtein/PublicHeader.h>
+
+
